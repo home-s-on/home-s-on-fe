b4bde1362ada71f2121822f0b11fe54c6a8848d4
Ahj-hj <ahj32@hanmail.net>
2024-11-28 15:12:48 +0900

    feat:AddTaskView #49-할일추가버튼common으로 변경

--- home-s-on-fe/View/Task/TaskListView.swift
+++ home-s-on-fe/View/Task/TaskListView.swift
@@ -13,53 +13,37 @@ struct TaskListView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                //프로필 영역
-                HStack(spacing: 12) {
-                    Image(systemName: "person.circle.fill")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(.gray)
-                    
-                    Text("ahn")
-                        .font(.system(size: 18, weight: .medium))
-                    
-                    Spacer()
-                }
-                .padding()
-                
-                // 할일 목록
-                ScrollView {
-                    LazyVStack(spacing: 12) {
-                        ForEach(viewModel.tasks) { task in
-                            TaskRowView(task: task)
-                                .padding(.horizontal)
-                        }
-                    }
-                }
-            }
-            .overlay(
+            ZStack {  // ZStack -> AddTaskButton 맨위에 오도록
                 VStack {
-                    Spacer()
-                    HStack {
+                    //프로필 영역
+                    HStack(spacing: 12) {
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.gray)
+                        
+                        Text("ahn")
+                            .font(.system(size: 18, weight: .medium))
+                        
                         Spacer()
-                        Button(action: {
-                        // 할일 추가
-                        }) {
-                            Image(systemName: "plus")
-                                .font(.title2)
-                                .foregroundColor(.white)
-                                .frame(width: 50, height: 50)
-                                .background(Color.blue)
-                                .clipShape(Circle())
-                                .shadow(radius: 4)
+                    }
+                    .padding()
+                    
+                    // 할일 목록
+                    ScrollView {
+                        LazyVStack(spacing: 12) {
+                            ForEach(viewModel.tasks) { task in
+                                TaskRowView(task: task)
+                                    .padding(.horizontal)
+                            }
                         }
-                        .padding()
                     }
                 }
-            )
-            .onAppear {
-                viewModel.fetchTasks(houseId: houseId)
+                .onAppear {
+                    viewModel.fetchTasks(houseId: houseId)
+                }
+                
+                AddTaskButton()
             }
         }
     }
